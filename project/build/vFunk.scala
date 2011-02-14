8c792afb432a57fa5de5c4874fecc44abf889e6e
Nycto <James@RoundEights.com>
2011-02-13 16:28:00 -0800

    Added sbt scaffolding

---
+++ project/build/vFunk.scala
@@ -0,0 +1,14 @@
+import sbt._
+
+class VFunkProject(info: ProjectInfo) extends DefaultProject(info)
+{
+    val specs = "specs" % "specs" % "1.6.7" from
+        "http://specs.googlecode.com/files/specs_2.8.1-1.6.7.jar"
+
+    val mockito = "mockito" % "mockito" % "1.8.5" from
+        "http://mockito.googlecode.com/files/mockito-all-1.8.5.jar"
+
+    val junit = "junit" % "junit" % "4.8.2" from
+        "https://github.com/downloads/KentBeck/junit/junit-4.8.2.jar"
+}
+
