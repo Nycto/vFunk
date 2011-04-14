150eb69ab08b46c2ddb52c8c5b7e43b3cc6beeae
Nycto <James@RoundEights.com>
2011-04-13 21:50:45 -0700

    Added an IPv6 validator

--- src/main/scala/vfunk/validate/Data.scala
+++ src/main/scala/vfunk/validate/Data.scala
@@ -42,10 +42,7 @@ class IPv4 extends Validator {
     lazy private val regexp = {
         val byte = """(?:(?:25[0-5])|(?:2[0-4][0-9])|(?:1[0-9]{2})|(?:[1-9]?[0-9]))"""
         val regex = List(
-            "^",
-            byte,
-            """(?:\.""" + byte + """){3}""",
-            "$"
+            "^", byte, """(?:\.""" + byte + """){3}""", "$"
         )
         regex.reduceLeft(_ + _).r
     }
@@ -57,3 +54,41 @@ class IPv4 extends Validator {
         }
     }
 }
+
+/**
+ * Validates an IPv6 address
+ */
+class IPv6 extends Validator {
+    lazy private val regexp = {
+        val hex = "[0-9a-fA-F]{1,4}"
+        val hexc = "(?:" + hex + ":)"
+        val chex = "(?::" + hex + ")"
+
+        val byte = """(?:(?:25[0-5])|(?:2[0-4][0-9])|(?:1[0-9]{2})|(?:[1-9]?[0-9]))"""
+        val ipv4 = "(?:" + byte + """(?:\.""" + byte + "){3})"
+
+        val regex = List(
+            "^",
+            "(?:",
+                "(", hexc, "{7}(", hex, "|:))",
+                "|(", hexc, "{6}(:", hex, "|", ipv4, "|:))",
+                "|(", hexc, "{5}((", chex, "{1,2})|:", ipv4, "|:))",
+                "|(", hexc, "{4}((", chex, "{1,3})|(", chex, "?:", ipv4, ")|:))",
+                "|(", hexc, "{3}((", chex, "{1,4})|(", chex, "{0,2}:", ipv4, ")|:))",
+                "|(", hexc, "{2}((", chex, "{1,5})|(", chex, "{0,3}:", ipv4, ")|:))",
+                "|((", hexc, "){1}((", chex, "{1,6})|(", chex, "{0,4}:", ipv4, ")|:))",
+                "|(:((", chex, "{1,7})|(", chex, "{0,5}:", ipv4, ")|:))",
+            ")",
+            "$"
+        )
+        regex.reduceLeft(_ + _).r
+    }
+
+    override def getErrors ( value: String ) = {
+        regexp.findFirstIn( value ) match {
+            case None => List( Err("IPV6", "Invalid IP address") )
+            case Some(_) => Nil
+        }
+    }
+}
+
