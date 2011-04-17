67f827d58fd34e663448356d48351b689ec25a1b
Nycto <James@RoundEights.com>
2011-04-16 18:58:47 -0700

    Added the filter definition

---
+++ src/main/scala/vfunk/filter/Definition.scala
@@ -0,0 +1,17 @@
+/**
+ * Structural code for defining the Filter interface
+ */
+
+package main.scala.vfunk.filter
+
+/**
+ * Applies a transformation to a string, resulting in another string
+ */
+trait Filter {
+
+    /**
+     * Applies a transformation and returns the result
+     */
+    def filter ( value: String ): String
+}
+
