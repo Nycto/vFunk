c40670aa28cf995ca8867556fd48cf1ae3e9853e
Nycto <James@RoundEights.com>
2011-12-17 16:13:34 -0800

    Added validator and filter builders

--- src/main/scala/vfunk/filter/Definition.scala
+++ src/main/scala/vfunk/filter/Definition.scala
@@ -5,6 +5,32 @@
 package com.roundeights.vfunk
 
 /**
+ * A builder for generating filters
+ */
+object Filter {
+
+    import com.roundeights.vfunk.filter._
+
+    def email = new EMail
+    def url = new URL
+    def ipv4 = new IPv4
+    def ipv6 = new IPv6
+    def identity = new Identity
+    def callback( callback: (String) => String ) = new Callback( callback )
+    def chain ( filters: Traversable[Filter] ) = new Chain( filters )
+    def chain ( filters: Filter* ) = new Chain( filters:_* )
+    def trim = new Trim
+    def alphaNum = new AlphaNum
+    def alpha = new Alpha
+    def digit = new Digit
+    def printable = new Printable
+    def characters ( chars: Set[Char] ) = new Characters( chars )
+    def characters ( chars: String ) = new Characters( chars )
+    def characters ( chars: Char* ) = new Characters( chars:_* )
+
+}
+
+/**
  * Applies a transformation to a string, resulting in another string
  */
 trait Filter {
---
+++ src/main/scala/vfunk/validate/Builder.scala
@@ -0,0 +1,62 @@
+package com.roundeights.vfunk
+
+/**
+ * Helper methods for creating validators
+ */
+object Validate {
+
+    import com.roundeights.vfunk.validate._
+    import scala.util.matching.Regex
+
+    def email = new Email
+    def ipv4 = new IPv4
+    def ipv6 = new IPv6
+    def minLength ( length: Int ) = new MinLength( length )
+    def maxLength ( length: Int ) = new MaxLength( length )
+    def exactLength ( length: Int ) = new ExactLength( length )
+    def notEmpty = new NotEmpty
+
+    def and( validators: List[Validator] ) = new And( validators )
+    def and( validators: Validator* ) = new And( validators:_* )
+
+    def or( validators: List[Validator] ) = new Or( validators )
+    def or( validators: Validator* ) = new Or( validators:_* )
+
+    def not (
+        validator: Validator,
+        message: String = "Value failed validation"
+    ) = new Not( validator, message )
+
+    def isNumeric = new IsNumeric
+    def odd = new Odd
+    def even = new Even
+
+    def equals ( vs: Number ) = new Equals( vs )
+    def lessThan ( vs: Number ) = new LessThan( vs )
+    def lessThanEquals ( vs: Number ) = new LessThanEquals( vs )
+    def greaterThan ( vs: Number ) = new GreaterThan( vs )
+    def greaterThanEquals ( vs: Number ) = new GreaterThanEquals( vs )
+
+    def manual( errors: List[Err] ) = new Manual( errors )
+    def manual( errors: Err* ) = new Manual( errors )
+
+    def invoke (callback: (String) => Traversable[Err]) = new Invoke(callback)
+
+    def in ( options: Set[String] ) = new In( options )
+    def in ( options: String* ) = new In( options:_* )
+
+    def alphaNum = new AlphaNum
+    def alpha = new Alpha
+    def digit = new Digit
+
+    def same ( versus: String, caseSensitive: Boolean = false )
+        = new Same(versus, caseSensitive)
+
+    def noWhitespace = new NoWhitespace
+    def regExp ( regex: Regex ) = new RegExp( regex )
+    def regExp ( regex: String ) = new RegExp( regex )
+    def notBlank = new NotBlank
+
+}
+
+
